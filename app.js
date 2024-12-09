6ee29d21372fdff8287a4e58a1af9675e038db7a
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-09 16:46:08 +0700

    Route handlers

--- app.js
+++ app.js
@@ -2,17 +2,51 @@ import express from 'express';
 const app = express()
 const port = 31000
 
-app.get('/users/:userId/books/:bookId', (req, res) => {
-    res.send(req.params)
+app.get('/example/a', (req, res) => {
+    res.send('Hello from A!')
 })
-app.get('/flights/:from-:to', (req, res) => {
-    res.send(req.params)
-})
-app.get('/plantae/:genus.:species', (req, res) => {
-    res.send(req.params)
-})
-app.get('/user/:userId(\d+)', (req, res) => {
-    res.send(req.params)
+
+app.get('/example/b',
+    (req, res, next) => {
+        console.log('the response will be sent by the next function ...')
+        next()
+    },
+    (req, res) => {
+        res.send('Hello from B!')
+    }
+)
+
+// const cb0 = function (req, res, next) {
+//     console.log('CB0')
+//     next()
+// }
+
+// const cb1 = function (req, res, next) {
+//     console.log('CB1')
+//     next()
+// }
+
+// const cb2 = function (req, res) {
+//     res.send('Hello from C!')
+// }
+
+// app.get('/example/c', [cb0, cb1, cb2])
+
+const cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+const cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+app.get('/example/d', [cb0, cb1], (req, res, next) => {
+    console.log('the response will be sent by the next function ...')
+    next()
+}, (req, res) => {
+    res.send('Hello from D!')
 })
 
 
