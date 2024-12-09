fe77f60deb009bb94014ab2081f977e29fa30c68
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-09 11:56:15 +0700

    app.all

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ import express from 'express';
 const app = express()
 const port = 31000
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
-  
+app.all('/secret', (req, res, next) => {
+    console.log('Accessing the secret section ...')
+    next() // pass control to the next handler
+})
+
+app.get('/secret', (req, res) => {
+    res.send('GET request to the secret page')
+})
+
+app.post('/secret', (req, res) => {
+    res.send('POST request to the secret page')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
