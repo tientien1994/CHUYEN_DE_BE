7e19a563d0d1100a1738901c519dde939388664b
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-09 11:06:01 +0700

    routing part 1

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express()
+const port = 31000
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
