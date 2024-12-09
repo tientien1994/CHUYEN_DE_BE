e4aea204ba6d493dda3c9c416263010a6afaf945
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-09 14:58:16 +0700

    routing with ?

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import express from 'express';
 const app = express()
 const port = 31000
 
-app.get('/', (req, res) => {
-    res.send('root')
+app.get('/ab?cd', (req, res) => { res.send('ab?cd') })
+app.get('/ab+cd', (req, res) => { res.send('ab+cd') })
+app.get('/ab*cd', (req, res) => { res.send('ab*cd') })
+app.get('/ab(cd)?e', (req, res) => { res.send('ab(cd)?e') })
+app.get(/a/, (req, res) => {
+    res.send('/a/')
 })
-app.get('/about', (req, res) => {
-    res.send('about')
-})
-app.get('/random.text', (req, res) => {
-    res.send('random.text')
+app.get(/.*fly$/, (req, res) => {
+    res.send('/.*fly$/')
 })
 
 app.listen(port, () => {
