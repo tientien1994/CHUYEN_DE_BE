34ef07ef16e310de1850a36c0fa7dddf3b694be9
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-10 16:44:14 +0700

    error handling

--- app.js
+++ app.js
@@ -2,33 +2,24 @@ import express from "express";
 const app = express();
 const port = 31000;
 
-const auththentications = function (req, res, next) {
-    const { role } = req.params
-    if(role === "admin"){
-        console.log('Người dùng là Admin, lập tức chuyển tới page hello world')
-        return next("route")
-    }
-    console.log(`Người dùng chỉ là ${role}`)
-    next()  
-}
-const requestTime = function (req, res, next) {
-    const currentTime = Date.now()
-    console.log("Thời gian hiện tại là: " + currentTime)
-    next()
-}
 
-app.get('/:role', [auththentications, requestTime], (req, res) => {
-    res.send('Hello World!')
+app.get('/exampleA', (req, res) => {
+    console.log(x)
+    res.send('Hello A!')
 })
 
+app.get('/exampleB', (req, res) => {
+    res.send('Hello B!')
+})
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Có lỗi xảy ra trong quá trình vận hành!')
+  })
+  app.use((req, res, next) => {
+    res.status(404).send('Không tìm thấy trang');
+});
 
-// app.use(requestTime)
 
-// app.get('/time', (req, res) => {
-//     let responseText = 'Hello World!<br>'
-//     responseText += `<small>Requested at: ${req.requestTime}</small>`
-//     res.send(responseText)
-// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
