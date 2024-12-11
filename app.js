47b8860a01565f138e255104980a6babd33a54d4
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-11 11:19:26 +0700

    do not user template engine

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const app = express()
 const port = 31000
+import path from 'path'
+const __dirname = path.resolve()
 
-
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/home.html")
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
