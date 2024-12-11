0fec4a4d9b15de4e686a9dec884317ea63a521d8
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-11 22:34:08 +0700

    menthod post

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 import express from "express";
 const app = express();
 const port = 31000;
+import bodyParser from "body-parser";
 
-app.get('/form', (req, res) => {
-    const queryString = req.query
-    
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.urlencoded())
+app.get('/form-post', (req, res) => {
     res.send(`
-        <form action="/form">
+        <form action="/form-post" method="post">
         <label for="fname">First name:</label><br>
         <input type="text" id="fname" name="fname" value="John"><br>
         <label for="lname">Last name:</label><br>
         <input type="text" id="lname" name="lname" value="Doe"><br><br>
         <input type="submit" value="Submit">
-        </form> 
-        <p>Du lieu duoc truyen vao la: ${req.query?.fname || ""} ${req.query?.lname || ""} </p>
-    `)
+        </form> `)
+})
+app.post('/form-post', (req, res) => {
+    const body = req.body
+    console.log({body})
+    res.send(`Du lieu nhan duoc la: ${body?.fname} ${body?.lname}`)
+
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
