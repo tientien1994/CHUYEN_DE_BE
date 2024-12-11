f294653455a8e5108e65ec1a4cfbbddf8abab8ff
GlifeTimmy <trandinhtien19941@gmail.com>
2024-12-11 17:18:50 +0700

    template engine 1

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ const app = express()
 const port = 31000
 import path from 'path'
 const __dirname = path.resolve()
+import ejs from 'ejs';
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/home.html")
+app.set('view engine', 'ejs')
+
+// ejs.delimiter = '?';
+app.get('/home', (req, res) => {
+    res.render("home", {
+        type: "trai_cay",
+    })
 })
+app.get('/category', (req, res) => {
+    res.render("category", {
+        name: "Trái cây trong nước",
+        listProduct: ["Cam", "Bưởi", "Xoài"],
+        listProduct2: [["Cam", "Bưởi", "Xoài"], ["1", "2", "3"], ["qq", "ww", "ee"]]
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
